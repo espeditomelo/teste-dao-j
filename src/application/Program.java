99141e611f4be5f268db187a390e916eeefcfbfb
Espedito Melo <espedito.melo@gmail.com>
2023-05-24 15:58:09 -0300

    add factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller s = new Seller(1, "josé", "jose@gmail.com", new Date(), 1000.0, obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println(s);
 	}
