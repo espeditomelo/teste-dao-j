169c8578f5d768cc890621d1139e7dca3b805281
Espedito Melo <espedito.melo@gmail.com>
2023-05-24 09:19:37 -0300

    class seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		
+		Seller s = new Seller(1, "josé", "jose@gmail.com", new Date(), 1000.0, obj);
 
-		System.out.println(obj);
+		System.out.println(s);
 	}
 
 }
