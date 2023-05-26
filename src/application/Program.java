90294a585dfb30951096e5bf8d0bd59686f3ad01
Espedito Melo <espedito.melo@gmail.com>
2023-05-26 06:18:41 -0300

    add findall

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,13 +14,19 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 	
-		System.out.println("---- Teste 1 : seller finById ----");				
+		System.out.println("\n---- Teste 1 : seller finById ----");				
 		Seller s1 = sellerDao.findById(3);
 		System.out.println(s1);
 		
-		System.out.println("---- Teste 1 : seller finByDepartment ----");
+		System.out.println("\n---- Teste 2 : seller finByDepartment ----");
 		Department d = new Department(1, null);
 		List<Seller> list = sellerDao.findByDepartment(d);
+		for (Seller s : list) {
+			System.out.println(s);
+		}
+		
+		System.out.println("\n---- Teste 3 : seller findAll ----");		
+		list = sellerDao.findAll();
 		for (Seller s : list) {
 			System.out.println(s);
 		}
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -90,8 +90,42 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as DepName " 
+					+ "FROM seller INNER JOIN department " 
+					+ "ON seller.DepartmentId = department.Id " 
+					+ "ORDER BY Name");
+
+			rs = st.executeQuery();
+		
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while (rs.next()) {
+				
+				Department d = map.get(rs.getInt("DepartmentId"));
+				
+				if (d == null) {
+					d = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), d);
+				}
+				
+				
+				Seller s = instantiateSeller(rs, d);
+				list.add(s);
+
+			}
+			return list;
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	@Override
